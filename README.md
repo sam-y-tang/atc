54b912f637700500c7a19d5672b83e064cf11465
Sam Tang <sam.y.tang@gmail.com>
2024-01-09 07:00:04 -0600

    ignore parsing error and continue

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # atc
+
 Scrapper to download all ATC data from [WHOCC](https://www.whocc.no/atc_ddd_index/)
 
-This is not the only code to do this work.  The noteable difference between the code provided here and other code examples is that this as been written in "off-the-shelf" R.  No additional packages are requried.  If you have R installed you should be able to run the script with no additional installs.
+This is not the only code to do this work. The noteable difference between the code provided here and other code examples is that this as been written in "off-the-shelf" R. No additional packages are requried. If you have R installed you should be able to run the script with no additional installs.
 
 The code will download the ATC data and write a .csv which can then be imported into to other projects as needed.
+
+It forked from https://github.com/dewittpe/atc.git. The original stopped on error in parsing H03CA code. I am not a programmer of R. Therefore I added code to make it continue.
+https://www.whocc.no/atc_ddd_index/?code=H03CA&showdescription=no
+
+"H03CA","Iodine therapy",NA,NA,NA,NA
+"","","0.33","g","O",NA
+"","","0.33","g","P",NA
--- atc_scrapper.R
+++ atc_scrapper.R
@@ -18,24 +18,30 @@ whocc_scrapper <- function(code) {
   code <- toupper(code)
   stopifnot(nchar(code) %in% c(1, 3, 4, 5, 7))
   stopifnot(
-    substr(code, 1, 1) %in% c("A", "B", "C", "D", "G", "H", "J",
-                              "L", "M", "N", "P", "R", "S", "V")
+    substr(code, 1, 1) %in% c(
+      "A", "B", "C", "D", "G", "H", "J",
+      "L", "M", "N", "P", "R", "S", "V"
+    )
   )
 
   target_url <-
-    paste0("https://www.whocc.no/atc_ddd_index/?code=", code,
-           "&showdescription=no")
+    paste0(
+      "https://www.whocc.no/atc_ddd_index/?code=", code,
+      "&showdescription=no"
+    )
 
   message(paste("Scrapping", target_url))
   site <- readLines(con = target_url)
 
   if (any(grepl("Invalid code", site))) {
-    rtn <- data.frame("ATC.code" = code,
-                      "Name" = "INVALID CODE",
-                      "DDD" = NA_real_,
-                      "U" = NA_character_,
-                      "Adm.R" = NA_character_,
-                      "Note" = NA_character_)
+    rtn <- data.frame(
+      "ATC.code" = code,
+      "Name" = "INVALID CODE",
+      "DDD" = NA_real_,
+      "U" = NA_character_,
+      "Adm.R" = NA_character_,
+      "Note" = NA_character_
+    )
     return(rtn)
   } else {
     # pattern to get ATC label for the given code
@@ -45,48 +51,64 @@ whocc_scrapper <- function(code) {
       pattern <- paste0(".+<a.*code=", code, "&showdescription=yes\">(.+)</a>.+")
     }
     label <- sub(pattern, "\\1", grep(pattern, site, value = TRUE))
-    rtn <- data.frame("ATC.code" = code,
-                      "Name" = label,
-                      "DDD" = NA_real_,
-                      "U" = NA_character_,
-                      "Adm.R" = NA_character_,
-                      "Note" = NA_character_)
+    rtn <- data.frame(
+      "ATC.code" = code,
+      "Name" = label,
+      "DDD" = NA_real_,
+      "U" = NA_character_,
+      "Adm.R" = NA_character_,
+      "Note" = NA_character_
+    )
 
     if (nchar(code) %in% c(1)) {
       child_pattern <-
         paste0(".+<a.*code=(", code, "[0-9][0-9])&.*>([A-Z]+.+)</a>.+")
-    } else if (nchar(code) %in% c(3, 4))  {
+    } else if (nchar(code) %in% c(3, 4)) {
       child_pattern <-
         paste0(".+<a.*code=(", code, "[A-Z])&.*>([A-Z]+.+)</a>.+")
     } else if (nchar(code) == 5) {
       if (any(grepl("<table", site))) {
-        tab <- paste(site[seq(grep("<table[^>]+>", site),
-                              grep("</table>", site), by = 1)],
-                     collapse = "")
-        rows <- length(gregexpr("<\\/tr>", tab)[[1]])
-        cols <- length(gregexpr("<\\/td>", tab)[[1]]) / rows
-        m <- gregexec("<td[^>]*>(.*?)</\\s*td>", tab)
-        cells <- regmatches(tab, m)[[1]][2, ]
-        cells <- gsub("&nbsp;", "", cells)
-        cells <- sub("<a[^>]+>(.*?)</a>", "\\1", cells)
-        cells <-
-          matrix(
-                 cells[-seq(cols, 1, by = -1)]
-                 , byrow = TRUE
-                 , dimnames = list(NULL,
-                                   make.names(cells[seq(1, cols, by = 1)]))
-                 , nrow = rows - 1
-                 , ncol = cols
-          )
-        cells <- as.data.frame(cells)
-        cells["DDD"][cells["DDD"] == ""] <- NA_character_
-        cells["U"][cells["U"] == ""] <- NA_character_
-        cells["Adm.R"][cells["Adm.R"] == ""] <- NA_character_
-        cells["Note"][cells["Note"] == ""] <- NA_character_
-        while (length(i <- which(cells["ATC.code"] == ""))) {
-          cells[i[1], "ATC.code"] <- cells[i[1] - 1, "ATC.code"]
-          cells[i[1], "Name"] <- cells[i[1] - 1, "Name"]
-        }
+        tryCatch(
+          {
+            tab <- paste(
+              site[seq(grep("<table[^>]+>", site),
+                grep("</table>", site),
+                by = 1
+              )],
+              collapse = ""
+            )
+            rows <- length(gregexpr("<\\/tr>", tab)[[1]])
+            cols <- length(gregexpr("<\\/td>", tab)[[1]]) / rows
+            m <- gregexec("<td[^>]*>(.*?)</\\s*td>", tab)
+            cells <- regmatches(tab, m)[[1]][2, ]
+            cells <- gsub("&nbsp;", "", cells)
+            cells <- sub("<a[^>]+>(.*?)</a>", "\\1", cells)
+            cells <-
+              matrix(
+                cells[-seq(cols, 1, by = -1)],
+                byrow = TRUE,
+                dimnames = list(
+                  NULL,
+                  make.names(cells[seq(1, cols, by = 1)])
+                ),
+                nrow = rows - 1,
+                ncol = cols
+              )
+            cells <- as.data.frame(cells)
+            cells["DDD"][cells["DDD"] == ""] <- NA_character_
+            cells["U"][cells["U"] == ""] <- NA_character_
+            cells["Adm.R"][cells["Adm.R"] == ""] <- NA_character_
+            cells["Note"][cells["Note"] == ""] <- NA_character_
+            while (length(i <- which(cells["ATC.code"] == ""))) {
+              cells[i[1], "ATC.code"] <- cells[i[1] - 1, "ATC.code"]
+              cells[i[1], "Name"] <- cells[i[1] - 1, "Name"]
+            }
+          },
+          # ignore error and continue
+          error = function(err) {
+
+          }
+        )
         return(rbind(rtn, cells))
       } else {
         return(NULL)
@@ -98,7 +120,7 @@ whocc_scrapper <- function(code) {
     child_codes <-
       sub(child_pattern, "\\1", grep(child_pattern, site, value = TRUE))
   }
- do.call(rbind, c(list(rtn), lapply(child_codes, whocc_scrapper)))
+  do.call(rbind, c(list(rtn), lapply(child_codes, whocc_scrapper)))
 }
 
 
@@ -110,8 +132,10 @@ whocc_scrapper <- function(code) {
 #
 atc_scrapper <- function(codes = "ALL") {
   if (length(codes) == 1 & all(codes == "ALL")) {
-    codes <- c("A", "B", "C", "D", "G", "H", "J",
-               "L", "M", "N", "P", "R", "S", "V")
+    codes <- c(
+      "A", "B", "C", "D", "G", "H", "J",
+      "L", "M", "N", "P", "R", "S", "V"
+    )
   }
 
   do.call(rbind, lapply(codes, whocc_scrapper))
@@ -143,4 +167,3 @@ write.csv(atc_codes, file = "atccodes.csv", row.names = FALSE)
 ################################################################################
 ###                               End of File                                ###
 ################################################################################
-
